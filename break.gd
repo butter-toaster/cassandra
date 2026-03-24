d941b929862d4a6338d5c9b916c703bab2e8c2f0
butter-toaster <giovannialt1134@gmail.com>
2026-03-23 22:15:19 -0400

    Image + SFX additions
    adding some of the new sprites and sounds for the ending scene

--- break.gd
+++ break.gd
@@ -26,7 +26,7 @@ func _on_pressed() -> void:
 				canpress = true
 			2:
 				canpress = false
-				await get_tree().create_timer(0.6).timeout
+				await get_tree().create_timer(2).timeout
 				%textleft.modulate = Color("8f63da")
 				%break.hide()
 				$".."._type("HELP ME-", %textleft, 1)
@@ -109,6 +109,16 @@ func _bargingsequence() -> void:
 	%bargingsfx.pitch_scale = randf_range(0.8, 0.55)
 	%bargingsfx.play()
 	await get_tree().create_timer(0.5).timeout
+	xscale = 16
+	yscale = 16
 	_screenshake()
-	%bargingsfx.pitch_scale = randf_range(0.5, 0.55)
-	%bargingsfx.play()
+	%breakingsfx.play()
+	%opendoor.show()
+	var policetween = create_tween().set_parallel(true)
+	policetween.tween_property(%officer1, "global_position:x", -300, 1.3)
+	policetween.tween_property(%officer2, "global_position:x", -300, 1.9)
+	policetween.tween_property(%officer3, "global_position:x", -300, 2.6)
+	policetween.tween_property(%endingnoisesfx,"volume_db", -10, 3)
+	
+	
+	
